c526ae597266ff48fb5350f32f2cd74bf3eaf7c1
My name <raveenda00@gmail.com>
2021-09-23 11:51:29 +0530

    test

--- src/main/java/it/uom/cse/MathOperation.java
+++ src/main/java/it/uom/cse/MathOperation.java
@@ -14,5 +14,27 @@ public class MathOperation {
     {
         return operand1 - operand2;
     }
+    public static int sumOfPrimes(n){
+	    int i, number, count, sum = 0
+
+			for(number = 1; number <= 100; number++)
+			{
+				count = 0;
+			    for (i = 2; i <= number/2; i++)
+			    {
+			    	if(number % i == 0)
+			    	{
+			    		count++;
+			    		break;
+			    	}
+			    }
+			    if(count == 0 && number != 1 )
+			    {
+			    	sum = sum + number;
+			    }
+			}
+             System.out.println("\n The Sum of Prime Numbers from 1 to 100 = " + sum);
+             }
+             }
 
 }
